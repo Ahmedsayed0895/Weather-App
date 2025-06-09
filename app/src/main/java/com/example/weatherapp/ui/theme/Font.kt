bfe5c6ccc01398fee24e8d78f0ab1f459df49aa7
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-09 05:45:10 +0300

    Define Urbanist font family
    This commit introduces the `urbanist` `FontFamily` in `Font.kt`.
    It includes the following font weights, referencing resources from `R.font`:
    - Normal (urbanist_regular)
    - Medium (urbanist_medium)
    - SemiBold (urbanist_semibold)
    - Bold (urbanist_bold)
    - ExtraBold (urbanist_extrabold)
    - Black (urbanist_black)
    - Light (urbanist_light)
    - Thin (urbanist_thin)
    - ExtraLight (urbanist_extralight)

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Font.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.weatherapp.R
+
+val urbanist = FontFamily(
+        Font(R.font.urbanist_regular,weight= FontWeight.Normal),
+        Font(R.font.urbanist_medium,weight= FontWeight.Medium),
+        Font(R.font.urbanist_semibold,weight= FontWeight.SemiBold),
+        Font(R.font.urbanist_bold,weight= FontWeight.Bold),
+        Font(R.font.urbanist_extrabold,weight= FontWeight.ExtraBold),
+        Font(R.font.urbanist_black,weight= FontWeight.Black),
+        Font(R.font.urbanist_light,weight= FontWeight.Light),
+        Font(R.font.urbanist_thin,weight= FontWeight.Thin),
+        Font(R.font.urbanist_extralight,weight= FontWeight.ExtraLight),
+
+)

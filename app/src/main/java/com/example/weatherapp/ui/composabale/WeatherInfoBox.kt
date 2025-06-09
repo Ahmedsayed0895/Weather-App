93ee9e0c43ed49d104c28f3c1b6996b6866413a3
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-10 02:47:27 +0300

    Create WeatherInfoBox Composable
    This commit introduces the `WeatherInfoBox` composable function.
    
    `WeatherInfoBox` is designed to display weather-related information, such as temperature, humidity, or wind speed. It takes an icon, a value, and a title as parameters.
    
    The layout consists of a `Box` with a white background and rounded corners. Inside the `Box`, a `Column` arranges the icon, value text, and title text vertically, centered horizontally.
    
    The text elements use the `urbanist` font family with specified font sizes, weights, and colors. The icon is tinted with the `DayPrimary` color.

---
+++ app/src/main/java/com/example/weatherapp/ui/composabale/WeatherInfoBox.kt
@@ -0,0 +1,68 @@
+package com.example.weatherapp.ui.composabale
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.DayPrimary
+import com.example.weatherapp.ui.theme.urbanist
+
+@Composable
+ fun WeatherInfoBox(
+    modifier: Modifier = Modifier,
+    icon: Painter,
+    value:String,
+    title:String,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(24.dp))
+            .background(Color.White)
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = CenterHorizontally
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = icon.toString(),
+                tint = DayPrimary
+
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = value,
+                fontFamily = urbanist,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.W500,
+                color = Color(0xDE060414),
+            )
+            Text(
+                text = title,
+                fontFamily = urbanist,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W400,
+                color = Color(0x99060414),
+            )
+
+        }
+
+    }
+}

4333781f22db62e05087ada279287bcd8640720f
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-11 13:30:55 +0300

    Refactor WeatherRepositoryImpl for error handling and client reuse
    This commit refactors `WeatherRepositoryImpl.kt` by:
    - Moving the `HttpClient` instantiation out of the `getWeatherData` function to be a class property. This allows the client to be reused across multiple calls, improving efficiency.
    - Adding a try-catch block within `getWeatherData` to handle potential exceptions during the API call or JSON deserialization. If an exception occurs, it is re-thrown as a generic `Exception`.

--- app/src/main/java/com/example/weatherapp/data/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/data/WeatherRepositoryImpl.kt
@@ -9,12 +9,17 @@ import io.ktor.client.statement.bodyAsText
 import kotlinx.serialization.json.Json
 
 class WeatherRepositoryImpl:WeatherRepository {
+    val client = HttpClient(CIO)
     val url = "https://api.open-meteo.com/v1/forecast?latitude=30.0626&longitude=31.2497&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,weather_code,pressure_msl,surface_pressure,wind_speed_10m&timezone=Africa%2FCairo"
     override suspend fun getWeatherData(): WeatherResponse {
-        val client = HttpClient(CIO)
-        val response = client.get(urlString = url)
-        val weatherResponse = Json.decodeFromString<WeatherResponse>(response.bodyAsText())
-        return weatherResponse
+        return try {
+            val response = client.get(urlString = url)
+            Json.decodeFromString<WeatherResponse>(response.bodyAsText())
+        }catch (e:Exception){
+         throw Exception(e)
+        }
+
+
     }
 }
 
